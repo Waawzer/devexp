4d5ae69a246b7c949f09cb8b20faee0a0043f538
Waawzer <waap3008@gmail.com>
2025-03-11 17:25:38 +0100

    16. Gestion erreur deplo

--- src/lib/services/notificationService.ts
+++ src/lib/services/notificationService.ts
@@ -1,30 +1,26 @@
 import Notification from '@/models/Notification';
 import dbConnect from '@/lib/dbConnect';
+import { Schema } from 'mongoose';
+
+interface NotificationParams {
+  type: 'mission_proposal' | 'application' | 'mission_application' | 'new_message' | 'mission_assigned';
+  from: Schema.Types.ObjectId | string;
+  to: Schema.Types.ObjectId | string;
+  projectId?: Schema.Types.ObjectId | string | undefined;
+  missionId?: Schema.Types.ObjectId | string | undefined;
+  messageId?: Schema.Types.ObjectId | string | undefined;
+  title: string;
+  message: string;
+}
 
 /**
  * Crée une notification
  */
-export async function createNotification({
-  type,
-  from,
-  to,
-  projectId = null,
-  missionId = null,
-  messageId = null,
-  title,
-  message,
-}) {
+export async function createNotification(params: NotificationParams) {
   await dbConnect();
   
   return await Notification.create({
-    type,
-    from,
-    to,
-    projectId,
-    missionId,
-    messageId,
-    title,
-    message,
+    ...params,
     status: 'pending',
     createdAt: new Date()
   });
